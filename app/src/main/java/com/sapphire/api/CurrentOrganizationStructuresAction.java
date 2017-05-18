37675c882ba8cfb19257d60aaa6e713319cce5f9
Dima <suvorov.dmitriy.ivanovich@gmail.com>
2017-05-18 19:01:17 +0300

    Task #1241 - Bug Maintenance - Part 7

---
+++ app/src/main/java/com/sapphire/api/CurrentOrganizationStructuresAction.java
@@ -0,0 +1,81 @@
+package com.sapphire.api;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import com.sapphire.R;
+import com.sapphire.Sapphire;
+import com.sapphire.logic.Environment;
+import com.sapphire.logic.NetRequests;
+import com.sapphire.logic.UserInfo;
+import com.sapphire.models.ErrorMessageData;
+import com.sapphire.models.ProfileData;
+import com.sapphire.models.ResponseData;
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.util.ArrayList;
+
+public class CurrentOrganizationStructuresAction extends AsyncTask{
+
+    public interface RequestCurrentOrganizationStructures {
+        public void onRequestCurrentOrganizationStructures(String result, ArrayList<ProfileData> datas);
+    }
+
+    private Context mContext;
+    private ArrayList<ProfileData> datas;
+
+    public CurrentOrganizationStructuresAction(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    protected Object doInBackground(Object... arg0) {
+        if (!NetRequests.getNetRequests().isOnline(true)) {
+            return Sapphire.getInstance().getResources().getString(R.string.text_need_internet);
+        }
+        String urlstring = Environment.SERVER + Environment.CurrentOrganizationStructuresURL;
+
+        ResponseData responseData = new ResponseData(NetRequests.getNetRequests().SendRequestCommon(urlstring,"",0,true,"GET",UserInfo.getUserInfo().getAuthToken()));
+
+        String result = "";
+
+        if (responseData.getSuccess()) {
+            JSONArray data = responseData.getData();
+            datas = new ArrayList<ProfileData>();
+            for (int y=0; y < data.length(); y++) {
+                try {
+                    ProfileData profileData = new ProfileData(data.getJSONObject(y));
+                    if (!profileData.getIsEmployee()) {
+                        continue;
+                    }
+                    datas.add(profileData);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            result = "OK";
+        } else {
+            ArrayList<ErrorMessageData> errorMessageDatas = responseData.getErrorMessages();
+            if (errorMessageDatas == null || errorMessageDatas.size() == 0) {
+                result = responseData.getHttpStatusMessage();
+            } else {
+                for (int y=0; y < errorMessageDatas.size(); y++) {
+                    if (!result.equals("")) {
+                        result = result + ". ";
+                    }
+                    result = errorMessageDatas.get(y).getName();
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        String resultData = (String) o;
+        if(mContext!=null) {
+            ((RequestCurrentOrganizationStructures) mContext).onRequestCurrentOrganizationStructures(resultData, datas);
+        }
+    }
+}
